108295797298421d6fb1ceadaa1fa3a2bb1a063c
Daniel Szczepaniak <danexq@gmail.com>
2022-10-09 23:52:58 +0200

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,71 @@ import styled from "styled-components";
 import "./App.css";
 import FormInputs from "./components/FormInputs";
 import React, { useCallback, useState } from "react";
+import { ImportsNotUsedAsValues } from "typescript";
 
 type FormDataProps = {
   username: string;
   email: string;
-  firstName: string;
-  fullName: string;
+  password: string;
+  comfirmPassword: string;
 };
 
 const initialState = {
   username: "",
   email: "",
-  firstName: "",
-  fullName: "",
+  password: "",
+  comfirmPassword: "",
 };
+
+export interface InputProp {
+  id: number;
+  name: string;
+  type: string;
+  placeholder: string;
+  errorMessage: string;
+  pattern?: string;
+}
 
 const App = () => {
   const [formData, setFormData] = useState<FormDataProps>(initialState);
+  const isDisabled = Object.values(formData).every((val) => val !== "");
+
+  const inputs: InputProp[] = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      errorMessage:
+        "Username should be 3-16 characters and shouldn't include any special character!",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      errorMessage: "It should be a valid email address!",
+    },
+    {
+      id: 3,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      errorMessage:
+        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
+      pattern:
+        "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$",
+    },
+    {
+      id: 4,
+      name: "comfirmPassword",
+      type: "password",
+      placeholder: "Comfirm Password",
+      errorMessage: "Passwords should match!",
+      pattern: formData.password,
+    },
+  ];
 
   const handleChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>): void => {
@@ -35,33 +83,18 @@ const App = () => {
 
   return (
     <AppContainer>
-      <form onSubmit={handleSubmit}>
-        <FormInputs
-          name="username"
-          placeholder="Username"
-          handleChange={handleChange}
-          value={formData.username}
-        />
-        <FormInputs
-          name="email"
-          placeholder="Email"
-          handleChange={handleChange}
-          value={formData.email}
-        />
-        <FormInputs
-          name="firstName"
-          placeholder="First Name"
-          handleChange={handleChange}
-          value={formData.firstName}
-        />
-        <FormInputs
-          name="fullName"
-          placeholder="Full Name"
-          handleChange={handleChange}
-          value={formData.fullName}
-        />
-        <button>Submit</button>
-      </form>
+      <StyledForm onSubmit={handleSubmit}>
+        <FormLabel>REGISTER</FormLabel>
+        {inputs.map((input) => (
+          <FormInputs
+            key={input.id}
+            {...input}
+            onChange={handleChange}
+            value={formData[input.name as keyof FormDataProps]}
+          />
+        ))}
+        <SubmitButton disabled={!isDisabled}>Submit</SubmitButton>
+      </StyledForm>
     </AppContainer>
   );
 };
@@ -73,4 +106,47 @@ const AppContainer = styled.div`
   align-items: center;
   justify-content: center;
   height: 100vh;
+  background: linear-gradient(to top, #2980b9, #6dd5fa);
+`;
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  border-radius: 1rem;
+  padding: 3rem 4rem;
+  gap: 2rem;
+`;
+
+const SubmitButton = styled.button`
+  width: 100%;
+  padding: 1rem 0;
+  border-radius: 2rem;
+  border: none;
+  background-color: rgba(41, 128, 185, 0.9);
+  color: white;
+  font-size: 1.5rem;
+  cursor: pointer;
+  transition: all 0.2s;
+  text-transform: uppercase;
+
+  &:disabled {
+    background-color: gray;
+  }
+
+  &:hover {
+    background-color: rgba(41, 128, 185, 1);
+  }
+
+  &:active {
+    transform: translateY(1px);
+  }
+`;
+
+const FormLabel = styled.h2`
+  color: #2980b9;
+  font-size: 3rem;
+  text-transform: uppercase;
 `;
--- src/components/FormInputs.tsx
+++ src/components/FormInputs.tsx
@@ -1,30 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { InputProp } from "../App";
 
-interface FormInputsProps {
-  placeholder: string;
-  name: string;
+interface FormInputsProps extends InputProp {
   value: string;
-  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const FormInputs = ({
-  placeholder,
-  name,
-  value,
-  handleChange,
-}: FormInputsProps) => {
+const FormInputs = (props: FormInputsProps) => {
+  const [focused, setFocused] = useState(false);
+  const { placeholder, errorMessage } = props;
+  const { ...inputProps } = props;
+
+  const handleFocus = () => {
+    setFocused(true);
+  };
+  console.log(...props);
+
   return (
-    <div className="formInput">
-      <Input
-        placeholder={placeholder}
-        type="text"
-        onChange={handleChange}
-        name={name}
-        value={value}
-        required
-      />
-    </div>
+    <FormContainer>
+      <label>{placeholder}</label>
+      <Input {...inputProps} />
+      <ErrorMessage>{errorMessage}</ErrorMessage>
+    </FormContainer>
   );
 };
 
@@ -32,9 +30,51 @@ const MemoizedFormInputs = React.memo(FormInputs);
 
 export default MemoizedFormInputs;
 
-const FormInput = styled.div``;
+const FormContainer = styled.div`
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 0.5rem;
+  font-size: 1.2rem;
+
+  label {
+    font-weight: 600;
+    color: rgba(0, 0, 0, 0.4);
+  }
+`;
 
 const Input = styled.input`
-  padding: 15px;
-  margin: 10px 0px;
+  padding: 1rem;
+  border: none;
+  border-radius: 10px;
+  font-size: inherit;
+  background-color: rgba(0, 0, 0, 0.03);
+  width: 100%;
+  transition: all 0.2s;
+
+  &::placeholder {
+    font-size: inherit;
+    color: rgba(0, 0, 0, 0.2);
+  }
+
+  &:active {
+    transform: translateY(1px);
+  }
+
+  &:focus {
+    outline: none;
+    background-color: rgba(0, 0, 0, 0.05);
+  }
+
+  &:invalid ~ span {
+    display: block;
+  }
+`;
+
+const ErrorMessage = styled.span`
+  padding: 3px;
+  color: red;
+  font-size: 12px;
+  display: none;
 `;
